ca7cbb6a1fa11bb6b7a4acc2291fb41b33deac44
Praful Rathore <rpraful@OJAS-IT.COM>
2018-12-20 10:22:26 +0530

    practice

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return '<h1>Hello Team! Welcome to Test HIV application using Flask.</h1>'
+def hello_world():
+    return 'Hello Team Flask'
+
 
 if __name__ == '__main__':
-    app.run(debug= True, port= 5004)
+    app.run()
